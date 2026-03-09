d47090fc5b4fcc3574b74a77a7a22e7595dea30c
Matheus-Leffa <matheusleffa@live.com>
2026-03-09 09:25:40 -0300

    Primeiro commit: Criação de pastas

---
+++ src/main/java/matheusleffa/telefoneCliente/TelefoneClienteApplication.java
@@ -0,0 +1,13 @@
+package matheusleffa.telefoneCliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelefoneClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelefoneClienteApplication.class, args);
+	}
+
+}
